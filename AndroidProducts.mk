a03e420199062fe3fd692fafd90cd1eb220a7a7f
mikedcoombs <mikedcoombs@outlook.com>
2023-06-03 20:33:12 -0700

    second commit

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_Doha_TMO.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_Doha_TMO-user \
+    omni_Doha_TMO-userdebug \
+    omni_Doha_TMO-eng
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-"# android_device_alcatel_5087Z" 
+# Android device tree for TCL 5087Z (Doha_TMO)
+
+```
+#
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+```
